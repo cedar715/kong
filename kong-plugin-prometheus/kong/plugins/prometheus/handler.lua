05cf2db8b9cbc09c72a8b0978446f04276742c8d
cedar715 <v.rao.jai@gmail.com>
2020-05-08 20:07:31 +0530

    Add files via upload

---
+++ kong-plugin-prometheus/kong/plugins/prometheus/handler.lua
@@ -0,0 +1,24 @@
+local prometheus = require "kong.plugins.prometheus.exporter"
+local basic_serializer = require "kong.plugins.log-serializers.basic"
+
+
+local kong = kong
+local timer_at = ngx.timer.at
+
+
+prometheus.init()
+
+
+local PrometheusHandler = {
+  PRIORITY = 13,
+  VERSION  = "0.6.0",
+}
+
+
+function PrometheusHandler:log(_)
+  local message = basic_serializer.serialize(ngx)
+  prometheus.log(message)
+end
+
+
+return PrometheusHandler
